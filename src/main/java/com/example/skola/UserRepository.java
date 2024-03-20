0b1eb0d913aeec9d4291ea22c51f478f44f72150
ReinisKoz <127198110+ReinisKoz@users.noreply.github.com>
2024-03-20 07:11:02 +0000

    backgroundAdded

--- src/main/java/com/example/skola/UserRepository.java
+++ src/main/java/com/example/skola/UserRepository.java
@@ -3,6 +3,8 @@ package com.example.skola;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
